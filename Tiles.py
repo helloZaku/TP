44b7e26340f8e6755a9b023096e74fac4a7d2b5e
Hello_Zaku <109777023+helloZaku@users.noreply.github.com>
2024-11-20 13:51:39 -0500

    Create Tiles.py

---
+++ Tiles.py
@@ -0,0 +1,15 @@
+class Tiles:
+
+    def __init__(self,cx,cy,width,height):
+        self.isInDetection = False
+        self.hasCharacter = False
+        self.hasObject = False
+        self.cx = cx
+        self.cy = cy
+        self.width = width
+        self.height = height
+
+    def draw(self):
+        if isInDetection = True:
+            drawRect(self.cx,self.cy,self.width,self.height,fill = 'yellow')
+        
