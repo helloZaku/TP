2fc00b36fa5f9bf20f0b4e37a4423e5c1c0b83d5
Hello_Zaku <109777023+helloZaku@users.noreply.github.com>
2024-11-24 10:28:43 -0500

    Update Tiles.py

--- Tiles.py
+++ Tiles.py
@@ -1,18 +1,24 @@
 from cmu_graphics import *
 class Tiles:
 
-    def __init__(self,cx,cy,width,height,hasCharacter = None,hasObject = None):
+    def __init__(self,left,top,width,height,Character = None,Object = None):
         self.isInDetection = False
-        self.hasCharacter = hasCharacter
-        self.hasObject = hasObject
-        self.cx = cx
-        self.cy = cy
+        self.Character = Character
+        self.Object = Object
+        self.left = left
+        self.top = top
         self.width = width
         self.height = height
+    
+    def __repr__(self):
+        return f'left = {self.left},top = {self.top}, width = {self.width}, height = {self.height}, character = {self.Character}'
 
     def draw(self):
-        if self.hasObject != None:
-            drawRect(self.cx,self.cy,self.width,self.height,fill = 'yellow')
-        elif self.hasCharacter != None:
-            drawRect(self.cx,self.cy,self.width,self.height,fill = 'red')) 
+        if self.Object != None:
+            #self.Object.draw
+            pass
+        elif self.Character != None:
+            self.Character.draw()
+        elif self.isInDetection == True:
+            drawRect(self.left,self.top,self.width,self.height,fill = 'yellow')
         
