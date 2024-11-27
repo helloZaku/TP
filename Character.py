fb90d789109e8c4dd54a594b5f28b7a508b9c03c
Hello_Zaku <109777023+helloZaku@users.noreply.github.com>
2024-11-27 10:20:03 -0500

    Update Character.py

--- Character.py
+++ Character.py
@@ -29,35 +29,36 @@ class Enemy(Character):
     def draw(self):
         super().draw()
 
+    #known feature: enemy teleport back to starting position when cannot move forward
     def patrol(self,app):
         if self.patrolLogic == 'straightVertical':
             if self.row == 0 or self.row == len(app.map) - 1:
-                self.dy = -1 * self.dy
+                self.dy = -self.dy
+            nextTile = app.map[self.row + self.dy][self.col]
+            if nextTile.character == None:
+                app.map[self.row][self.col].character = None 
+                self.row += self.dy
+                self.top += self.dy * self.height
+                app.map[self.row][self.col].character = self
             else:
-                tile = app.map[self.row + self.dy][self.col]
-                if tile.character == None:
-                    app.map[self.row][self.col].character = None 
-                    self.row += self.dy
-                    self.top += self.dy * self.height
-                    app.map[self.row][self.col].character = self
-                else:
-                    self.dy = -1 * self.dy
-            
+                self.dy = -self.dy
 
         elif self.patrolLogic == 'straightHorizontal':
-            if self.col == 0 or self.row == len(app.map[0]) - 1:
-                self.dx = -1 * self.dx
+            if self.col == 0 or self.col == len(app.map[0]) - 1:
+                self.dx = -self.dx
+
+            nextTile = app.map[self.row][self.col + self.dx]
+            if nextTile.character == None:
+                app.map[self.row][self.col].character = None 
+                self.col += self.dx
+                self.left += self.dx * self.width
+                app.map[self.row][self.col].character = self
             else:
-                tile = app.map[self.row][self.col + self.dx]
-                if tile.character == None:
-                    app.map[self.row][self.col].character = None 
-                    self.col += self.dx
-                    self.left += self.dx * self.width
-                    app.map[self.row][self.col].character = self
-                else:
-                    self.dx = -1 * self.dx
+                self.dx = -self.dx
         
     
+
+
 
 class Player(Character):
     def __init__(self,name,left=0,top=0,width=0,height=0,row=0,col=0):
