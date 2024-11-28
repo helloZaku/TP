9539ebf7108e5fa72e351f5e6550707380be5501
Hello_Zaku <109777023+helloZaku@users.noreply.github.com>
2024-11-28 14:24:51 -0500

    Update Tiles.py

--- Tiles.py
+++ Tiles.py
@@ -5,9 +5,9 @@ from Node import *
 class Tiles:
 
     def __init__(self,left,top,width,height,Character = None,Object = None):
-        self.isInDetection = False
+        self.isInFOV = False
         self.character = Character
-        self.Object = Object
+        self.object = Object
         self.left = left
         self.top = top
         self.width = width
@@ -16,11 +16,11 @@ class Tiles:
     def __repr__(self):
         return f'left = {self.left},top = {self.top}, width = {self.width}, height = {self.height}, character = {self.character}'
 
-    def draw(self):
-        if self.Object != None:
-            self.Object.draw
+    def draw(self,app):
+        if self.object != None:
+            self.object.draw(app)
         elif self.character != None:
-            self.character.draw()
-        elif self.isInDetection == True:
+            self.character.draw(app)
+        elif self.isInFOV == True:
             drawRect(self.left,self.top,self.width,self.height,fill = 'yellow')
         
