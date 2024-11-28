80d5851547b665764da240f51b5aaf3e11800f9c
Hello_Zaku <109777023+helloZaku@users.noreply.github.com>
2024-11-28 14:25:03 -0500

    Update Object.py

--- Object.py
+++ Object.py
@@ -9,7 +9,7 @@ class Object():
         self.row = row
         self.col = col
     
-    def draw(self):
+    def draw(self,app):
         drawCircle(self.left,self.top,self.width/2,fill = 'black')
 
 class Wall(Object):
@@ -19,8 +19,8 @@ class Wall(Object):
     def __repr__(self):
         return f'Wall'
     
-    def draw(self):
-        drawRect(self.left,self.top,self.width,self.height,fill = 'brown')
+    def draw(self,app):
+        drawRect(self.left,self.top,self.width,self.height,fill = 'purple')
     
 class Dumpster(Object):
     def __init__(self,left,top,width,height,row,col):
@@ -29,19 +29,19 @@ class Dumpster(Object):
     def __repr__(self):
         return f'Dumpster'
     
-    def draw(self):
+    def draw(self,app):
         drawRect(self.left,self.top,self.width,self.height,fill = 'green')
-        drawCircle(self.left,self.top,self.width/2,fill = 'white')
+        drawCircle(self.left + self.width/2,self.top + self.width/2,self.width/2,fill = 'white',align = 'center')
     
 class Door(Object):
     def __init__(self,left,top,width,height,row,col):
         super().__init__(left,top,width,height,row,col)
         self.isClosed = True
     
-    def draw(self):
+    def draw(self,app):
         if self.isClosed == True:
             drawRect(self.left,self.top,self.width,self.height,fill=None, border='grey',
-            borderWidth=self.width / 10)
+            borderWidth=self.width / 5)
         
         else:
             drawRect(self.left,self.top,self.width,self.height,fill='grey')
