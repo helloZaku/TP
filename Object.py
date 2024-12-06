7732c20226d3735f689ac4f65b24968c714f57de
Hello_Zaku <109777023+helloZaku@users.noreply.github.com>
2024-12-06 01:01:08 -0500

    Update Object.py

--- Object.py
+++ Object.py
@@ -20,29 +20,9 @@ class Wall(Object):
         return f'Wall'
     
     def draw(self,app):
-        drawRect(self.left,self.top,self.width,self.height,fill = 'purple')
-    
-class Dumpster(Object):
-    def __init__(self,left,top,width,height,row,col):
-        super().__init__(left,top,width,height,row,col)
-
-    def __repr__(self):
-        return f'Dumpster'
-    
-    def draw(self,app):
-        drawRect(self.left,self.top,self.width,self.height,fill = 'green')
-        drawCircle(self.left + self.width/2,self.top + self.width/2,self.width/2,fill = 'white',align = 'center')
-    
-class Door(Object):
-    def __init__(self,left,top,width,height,row,col):
-        super().__init__(left,top,width,height,row,col)
-        self.isClosed = True
-    
-    def draw(self,app):
-        if self.isClosed == True:
-            drawRect(self.left,self.top,self.width,self.height,fill=None, border='grey',
-            borderWidth=self.width / 5)
         
-        else:
-            drawRect(self.left,self.top,self.width,self.height,fill='grey')
+        
+        drawImage(app.wall,self.left,self.top,width=self.width, height=self.height)
+    
+
     
